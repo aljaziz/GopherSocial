19ca55647140a338ef28a082eeb5da07d9867962
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-04-24 23:37:59 +0600

    query timeout added

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -4,25 +4,33 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var (
-	ErrNotFound = errors.New("record not found")
+	ErrNotFound          = errors.New("record not found")
+	QueryTimeoutDuration = 5 * time.Second
 )
 
 type Storage struct {
 	Posts interface {
 		Create(context.Context, *Post) error
 		GetByID(context.Context, int64) (*Post, error)
+		Update(context.Context, *Post) error
+		Delete(context.Context, int64) error
 	}
 	Users interface {
 		Create(context.Context, *User) error
+	}
+	Comments interface {
+		GetByPostID(context.Context, int64) ([]Comment, error)
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostStore{db},
-		Users: &UserStore{db},
+		Posts:    &PostStore{db},
+		Users:    &UserStore{db},
+		Comments: &CommentStore{db},
 	}
 }
