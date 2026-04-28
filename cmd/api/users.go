e28cf7a216a3f5f0f5afb6486adad1fb00393870
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-04-28 20:22:51 +0600

    follow and unfollow handler added

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -8,6 +8,10 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type userKey string
+
+const userContextKey userKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
 	if err != nil {
@@ -33,4 +37,57 @@ func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 		app.internalServerErrorResponse(w, r, err)
 		return
 	}
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+
+	followedID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Followers.Follow(ctx, followedID, followerUser.ID); err != nil {
+		switch err {
+		case store.ErrNotFound:
+			app.conflictResponse(w, r, err)
+			return
+		default:
+			app.internalServerErrorResponse(w, r, err)
+			return
+		}
+	}
+	if err := jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+
+	unfollowedID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Followers.Unfollow(ctx, followerUser.ID, unfollowedID); err != nil {
+		app.internalServerErrorResponse(w, r, err)
+		return
+	}
+
+	if err := jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerErrorResponse(w, r, err)
+	}
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userContextKey).(*store.User)
+	return user
 }
