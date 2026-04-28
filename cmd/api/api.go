fa84dc542290f7cf932e6f034ff5c4502cec7fb1
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-04-28 20:22:18 +0600

    follow and unfollow routes added

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -51,9 +51,11 @@ func (app *application) mount() http.Handler {
 				r.Delete("/", app.deletePostHandler)
 			})
 		})
-		r.Route("/users", func(r chi.Router){
-			r.Route("/{userID}", func(r chi.Router){
+		r.Route("/users", func(r chi.Router) {
+			r.Route("/{userID}", func(r chi.Router) {
 				r.Get("/", app.getUserHandler)
+				r.Put("/follow", app.followUserHandler)
+				r.Put("/unfollow", app.unfollowUserHandler)
 			})
 		})
 	})
