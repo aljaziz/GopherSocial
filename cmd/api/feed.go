e11b5a565e1d425eba13c0b48ec97c7e39053483
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-05-11 19:06:27 +0600

    pagination added

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,10 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/aljaziz/GopherSocial/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	// pagination, fillters, sort
+	fq := store.PaginationFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+		Tags:   []string{},
+		Search: "",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(42))
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(42), fq)
 	if err != nil {
 		app.internalServerErrorResponse(w, r, err)
 		return
